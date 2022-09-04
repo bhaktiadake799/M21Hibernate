890c3a1486c2316370ed43d0c88ed94d6048d9b5
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-09-04 11:31:38 +0530

    commit1

---
+++ Day 4 many to one Relationship/src/com/cg/association3/Department.java
@@ -0,0 +1,29 @@
+package com.cg.association3;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="departmant")
+
+public class Department {
+	@Id
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
